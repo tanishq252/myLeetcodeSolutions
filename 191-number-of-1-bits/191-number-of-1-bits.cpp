dca723b286df7cf6950affe08f1d91982dc367b7
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-03-04 09:08:45 +0530

    Time: 0 ms (100.00%), Space: 6 MB (48.01%) - LeetHub

---
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int c = 0;
+        while(n>0){
+            if(n%2 == 1){c++;}
+            n = n/2;
+        }
+        return c;
+    }
+};
