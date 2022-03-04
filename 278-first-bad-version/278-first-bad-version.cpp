3b47dd8f75effc129776139bcd1e323dca582b9c
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-03-04 21:05:33 +0530

    Time: 5 ms (12.00%), Space: 6 MB (23.57%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.cpp
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -4,10 +4,15 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        int fina;
-        while(isBadVersion(n)){
-            fina = n--;
+        int low = 0;
+        int high = n;
+                int mid = low + (high-low)/2;
+
+        while(high>=low){
+            if(!isBadVersion(mid)) {low = mid+1;}
+            else {high = mid-1;}
+            mid = low+(high-low)/2;
         }
-        return fina;
+        return mid;
     }
 };
