1942232f1b1142de97198f4a643f8c08d81ccfc8
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-03-04 19:43:46 +0530

    Time: 56 ms (9.87%), Space: 43.4 MB (7.34%) - LeetHub

---
+++ 799-champagne-tower/799-champagne-tower.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    double champagneTower(int poured, int query_row, int query_glass) {
+        vector<vector<double>> v( 102 , vector<double> (102, 0));
+        v[0][0] = (double)poured;
+        for(int i=0;i<=query_row;i++){
+            for(int j=0;j<=query_glass;j++){
+                double flow = (v[i][j]-1)/2;
+                // as we want equal amount of water flowing after filling the current glass
+                if(flow>0){v[i+1][j] += flow;
+                v[i+1][j+1] += flow;}
+            }
+        }
+        
+        return (v[query_row][query_glass]>1)?1:v[query_row][query_glass];
+
+    }
+};
