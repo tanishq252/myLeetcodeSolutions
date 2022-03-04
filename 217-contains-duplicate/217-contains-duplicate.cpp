07eb59cae965547fa3114f7206e65310875e0991
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-03-04 13:42:05 +0530

    Time: 115 ms (67.72%), Space: 46.4 MB (97.06%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        if(nums.size() == 1){return false;}
+        sort(nums.begin(), nums.end());
+        for(int i=1;i<nums.size();i++){
+            if(nums[i] == nums[i-1]){return true;}
+        }
+        return false;
+    }
+};
