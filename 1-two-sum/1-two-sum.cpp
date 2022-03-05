875cdf62f57ed65cb63d6324a8ad3ae14c68bcff
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-03-05 19:28:07 +0530

    Time: 716 ms (14.93%), Space: 10.2 MB (67.11%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -6,6 +6,29 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> v;
+        // int i= 0;
+        // int j =nums.size()-1 ;
+        // map<int, int> m;
+        // for(int k = 0;k<nums.size();k++){
+        //     m[nums[k]] = k;
+        // }
+        // sort(nums.begin(), nums.end());
+        // while(i<j){
+        //     if(nums[i]+nums[j] == target && i!=j){
+        //         v.push_back(m[nums[i]]);
+        //         v.push_back(m[nums[j]]);
+        //         return v;
+        //     }
+        //     else if(nums[i]+nums[j] > target){
+        //         j--;
+        //     }
+        //     else if(nums[i]+nums[j] < target){
+        //         i++;
+        //     }
+        // }
+        
+        
+//         passed test cases but wasn't solved with 2 pointers method
         for(int i=0;i<nums.size();i++){
             for (int j=0;j<nums.size();j++){
                 if(nums[i]+nums[j]== target && (i!=j)){
