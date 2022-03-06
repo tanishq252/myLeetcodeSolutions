754be7e62b30fafc244f1b2912110beb0d7059f0
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-03-06 19:01:53 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (96.78%) - LeetHub

---
+++ 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> sumZero(int n) {
+        vector<int> v;
+        if(n%2!=0){v.push_back(0);}
+        for(int i=1;i<=n/2;i++){
+            v.push_back(i);
+                        v.push_back(-1*i);
+
+        }
+        return v;
+    }
+};
