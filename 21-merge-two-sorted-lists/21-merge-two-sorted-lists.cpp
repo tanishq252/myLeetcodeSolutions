a3cfbd3d9201c3c03037c5d1cf8d31ba687c2fc5
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-03-07 12:20:34 +0530

    Time: 14 ms (34.90%), Space: 14.8 MB (45.55%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -11,36 +11,56 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* t1, ListNode* t2) {
-        ListNode* newList = NULL;
+        
+//         recursion
+        
         if(t1==NULL){return t2;}
         else if(t2 == NULL){return t1;}
-
+        
+        
         if(t1->val<t2->val){
-            newList = t1;
-            t1 = t1->next;
-        }else{
-            newList = t2;
-            t2 = t2->next;
+            t1->next = mergeTwoLists(t1->next, t2);
+            return t1;
         }
-        ListNode* temp = newList;
-        while(t1!=NULL && t2!=NULL){
-            if(t1->val < t2->val){
-                temp->next = t1;
-                t1 = t1->next;
-
-            }else{
-                temp->next = t2;
-                t2 = t2->next;
-                cout<<"oo"<<"\n";
-            }
-            temp = temp->next;
+        else{
+            t2->next = mergeTwoLists(t2->next, t1);
+            return t2;
         }
         
-        if(t1){
-            temp->next = t1;
-        }else{
-            temp->next = t2;
-        }
-        return newList;
+        
+        
+//          USING BASIC LOOPS   
+        
+//         ListNode* newList = NULL;
+//         if(t1==NULL){return t2;}
+//         else if(t2 == NULL){return t1;}
+
+//         if(t1->val<t2->val){
+//             newList = t1;
+//             t1 = t1->next;
+//         }else{
+//             newList = t2;
+//             t2 = t2->next;
+//         }
+//         ListNode* temp = newList;
+//         while(t1!=NULL && t2!=NULL){
+//             if(t1->val < t2->val){
+//                 temp->next = t1;
+//                 t1 = t1->next;
+
+//             }else{
+//                 temp->next = t2;
+//                 t2 = t2->next;
+//                 cout<<"oo"<<"\n";
+//             }
+//             temp = temp->next;
+//         }
+        
+//         if(t1){
+//             temp->next = t1;
+//         }else{
+//             temp->next = t2;
+//         }
+//         return newList;
     }
 };
