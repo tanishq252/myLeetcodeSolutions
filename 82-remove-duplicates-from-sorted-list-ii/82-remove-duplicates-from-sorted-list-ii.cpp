d278f90795b34b6f78d5ed512870b21c187aedc5
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-03-09 10:45:00 +0530

    Time: 9 ms (61.61%), Space: 12 MB (6.73%) - LeetHub

--- 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.cpp
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.cpp
@@ -11,23 +11,32 @@
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-         ListNode *temp = head;
+        // temp to traverse through the linked list
+        ListNode *temp = head;
+        // map to store frequencies of linked list
         map<int, int> m;
+        // traversing through linkedlist to record data 
         while(temp){
             m[temp->val]++;
             temp = temp->next;
         }
-        temp = head;
+        
+        // ListNode t is created to traverse through the given list again to check frequencies
         ListNode* t = head;
+        // new List to return values as per the problem statement
         ListNode* newL = NULL;
 
         while(t){
+            // checking whether the value of current node is appeared only once
             if(m[t->val] == 1){
+                //creating a new instance of node to initialize the value and point it to NULL
                 ListNode *tt = new ListNode;
                 tt->val = t->val;
                 tt->next = NULL;
+                // if it is first node then making it head of the list
                 if(newL == NULL){newL = tt;}
                 else{
+                    // if the list consists of head then we have to traverse to last and append the data to tail as list is already sorted
                     ListNode* trave = newL;
                     while(trave->next!=NULL){
                         trave = trave->next;
@@ -35,6 +44,7 @@ public:
                     trave->next = tt;
                 }
             }
+            // moving to the next node
             t = t->next;
         }
 
