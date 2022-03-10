d5ff72303825d22ac8cebf8a5316a2f1768e67e6
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-03-10 12:45:06 +0530

    Time: 34 ms (79.33%), Space: 71.5 MB (11.57%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -11,30 +11,22 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        long long int n1=0,n2=0;
-        long long int j1=1, j2=1;
         ListNode *t1 = l1;
         ListNode *t2 = l2;
         ListNode *newlist = new ListNode();
         ListNode *ptr = newlist;
-        int add = 0, add_now =0;
+        int add = 0;
         while(t1&&t2){
-            // cout<<t1->val<<" "<<t2->val;
-            
             int sum = (t1->val+t2->val+add)%10;
-            cout<<sum<<" ";
             ListNode* newN = new ListNode(sum);
             ptr->next =newN;
             ptr = ptr->next;
             add = (t1->val+t2->val+add)/10;
-            // cout<<add;
             t1 = t1->next;
             t2 = t2->next;
         }
         while(t1){
-           // cout<<t1->val;
             int sum = (t1->val+add)%10;
-            cout<<sum<<" ";
             ListNode* newN = new ListNode(sum);
             ptr->next =newN;
             ptr = ptr->next;
@@ -42,9 +34,7 @@ public:
             t1 = t1->next;
         }
         while(t2){
-           // cout<<t2->val;
             int sum = (t2->val+add)%10;
-            cout<<sum<<" ";
             ListNode* newN = new ListNode(sum);
             ptr->next =newN;
             ptr = ptr->next;
@@ -56,7 +46,6 @@ public:
             ptr->next =newN;
             ptr = ptr->next;
         }
-        
         return newlist->next;
     }
 };
