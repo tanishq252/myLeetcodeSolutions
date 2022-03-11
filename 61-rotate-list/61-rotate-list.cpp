a74e7059a1ab00f3b75a0aa5ac3b3650b95d93c5
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-03-11 07:49:11 +0530

    Time: 7 ms (82.36%), Space: 11.8 MB (6.63%) - LeetHub

--- 61-rotate-list/61-rotate-list.cpp
+++ 61-rotate-list/61-rotate-list.cpp
@@ -11,30 +11,54 @@
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
+        // passed all test cases with runtime 4 ms
+        // ListNode *temp = head;
+        // vector<int> v1;
+        // vector<int> v2;
+        // ListNode *newList = new ListNode();
+        // ListNode *ptr = newList;
+        // while(temp){
+        //     v1.push_back(temp->val);            
+        //     temp = temp->next;
+        // }
+        // int n = v1.size();
+        // if(n == 0){return head;}
+        // k%=n;
+        // for(int i=n-k;i<n;i++){
+        //     v2.push_back(v1[i]);
+        // }
+        // for(int i=0;i<n-k;i++){
+        //     v2.push_back(v1[i]);
+        // }
+        // for(int i=0;i<v2.size();i++){
+        //     ListNode *newNode = new ListNode(v2[i]);
+        //     ptr->next = newNode;
+        //     ptr = ptr->next;
+        // }
+        // return newList->next;
+        
         ListNode *temp = head;
-        vector<int> v1;
-        vector<int> v2;
+        ListNode *prev = head;
+        int len = 1;
+        if(head==NULL || head->next==NULL){return head;}
         ListNode *newList = new ListNode();
         ListNode *ptr = newList;
-        while(temp){
-            v1.push_back(temp->val);            
+        while(temp->next){
+            len++;            
             temp = temp->next;
         }
-        int n = v1.size();
-        if(n == 0){return head;}
-        k%=n;
-        for(int i=n-k;i<n;i++){
-            v2.push_back(v1[i]);
+        temp->next = head;
+        cout<<len<<" ";
+        k%=len;
+        cout<<k;
+        temp = head;
+        int j = (len-k);
+        while(j--){
+            prev= temp;
+            temp = temp->next;
         }
-        for(int i=0;i<n-k;i++){
-            v2.push_back(v1[i]);
-        }
-        for(int i=0;i<v2.size();i++){
-            ListNode *newNode = new ListNode(v2[i]);
-            ptr->next = newNode;
-            ptr = ptr->next;
-        }
-        return newList->next;
-        
+        head = temp;
+        prev->next = NULL;
+        return head;
     }
 };
