8802785050493426ca5fb30aeebb352f36ff9ed7
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-03-12 23:09:38 +0530

    Time: 0 ms (100.00%), Space: 10.7 MB (75.63%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
@@ -21,14 +21,43 @@ public:
             v.push_back(temp->val);
             temp = temp->next;
         }
-        cout<<len;
         temp = head;
-        for(int i=0;i<len;i++){
-            if(i!=(len - n)){ListNode *newNode = new ListNode(temp->val);
-            ptr->next = newNode;
-            ptr = ptr->next;}
+        if(len == n){return head->next;}
+        len-=1;
+        if(len == 0){return NULL;}
+        // if(len == )
+        while(temp){
+            cout<<len;
+            if(len == n){
+                temp->next = temp->next->next;
+                break;
+            }
             temp = temp->next;
+            len--;
         }
-        return newList->next;
+        
+        return head;
+        
+        
+//         passed all test cases
+        // ListNode *temp = head;
+        // ListNode *newList = new ListNode();
+        // ListNode *ptr = newList;
+        // int len = 0;
+        // vector<int> v;
+        // while(temp){
+        //     len++;
+        //     v.push_back(temp->val);
+        //     temp = temp->next;
+        // }
+        // cout<<len;
+        // temp = head;
+        // for(int i=0;i<len;i++){
+        //     if(i!=(len - n)){ListNode *newNode = new ListNode(temp->val);
+        //     ptr->next = newNode;
+        //     ptr = ptr->next;}
+        //     temp = temp->next;
+        // }
+        // return newList->next;
     }
 };
