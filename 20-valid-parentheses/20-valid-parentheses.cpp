b10ac268fdf191826906d1aae7a001d99c80e6ee
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-03-13 08:56:37 +0530

    Time: 3 ms (35.76%), Space: 6.3 MB (51.50%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        if(s.length() == 1){return false;}
+        if(s[0] == '}'||s[0] == ']'||s[0] == ')'){return false;}
+        for(int i=0;i<s.length();i++){
+            if(s[i] == '('||s[i] == '['||s[i] == '{'){
+                st.push(s[i]);
+            }
+            else if(st.empty() && (s[i] == '}'||s[i] == ']'||s[i]== ')')){
+                return false;
+            }
+            else{
+                char top = st.top();
+                if(st.empty()){return false;}
+                else {
+                    if((s[i] == ')'&&top == '(')||(s[i] == ']'&&top == '[')||(s[i] == '}'&&top == '{')){
+                        st.pop();
+                        }
+                    else if(top == '}'||top == ']'||top == ')'){
+                        return false;
+                    }
+                    else{
+                        return false;
+                    }
+                }
+            }
+        }
+        if(st.empty()){return true;}
+        return false;
+    }
+};
