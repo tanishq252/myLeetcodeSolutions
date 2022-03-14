28c02d2285bb6f996e119fb7ad8262d0705ffa59
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-03-14 22:25:24 +0530

    Time: 8 ms (62.74%), Space: 7.8 MB (80.57%) - LeetHub

--- 71-simplify-path/71-simplify-path.cpp
+++ 71-simplify-path/71-simplify-path.cpp
@@ -1,29 +1,39 @@
 class Solution {
 public:
     string simplifyPath(string path) {
-        string s;
+        string s="/";
         if(path=="/"||path=="/.."||path=="/."){return "/";}
         vector<string> v;
+        path+="/";
         string t="";
         for(int i=0;i<path.length();i++){
-            t="";
-            while(i<path.length()&&path[i]!='/'){
-                t+=path[i]; i++;
-            }
-            if(t==".."){
-                if(v.empty()==false){
-                    v.pop_back();
+            if(path[i]=='/'){
+                if(t == ".."){
+                    if(v.size()>0){
+                        v.pop_back();
+                    }
                 }
+                else if(t == "."||t == ""){
+                }
+                else{
+                    v.push_back(t);
+                }
+                t = "";
             }
-            else if(t==""||t=="."){}
             else{
-                v.push_back(t);
+                t+=path[i];
             }
         }
-        for(int i=0;i<v.size();i++){
-            s+= "/"+v[i];
-        }
+        cout<<t;
         
-        return (s=="")?"/" : s;
+        if(v.size() == 0){return "/";}
+        
+        for(int i=0;i<v.size()-1;i++){
+            s+=v[i];
+            s+="/";
+        }
+        s+=v[v.size()-1];
+        
+        return s;
     }
 };
