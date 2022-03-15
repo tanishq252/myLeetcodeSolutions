6d764dfcc32cfcee94115e31e808dd74340a96d8
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-03-15 18:39:21 +0530

    Time: 20 ms (90.23%), Space: 23.3 MB (42.21%) - LeetHub

--- 344-reverse-string/344-reverse-string.cpp
+++ 344-reverse-string/344-reverse-string.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        reverse(s.begin(), s.end());
+        // reverse(s.begin(), s.end());
+        int i=0;
+        int j = s.size()-1;
+        while(i<j){
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;j--;
+        }
+        // return s;
     }
 };
