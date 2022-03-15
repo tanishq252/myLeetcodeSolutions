75e1b2f75e9d9c6de651e6a30824c021a6061b80
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-03-15 18:05:59 +0530

    Time: 55 ms (67.04%), Space: 18.1 MB (30.11%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int maximumUnits(vector<vector<int>>& boxTypes, int target) {
         vector<pair<int, int>> v;
-        int sumB=0;
         int ans=0;
         for(auto i:boxTypes){
             v.push_back(make_pair(i[1], i[0]));
@@ -10,16 +9,16 @@ public:
         sort(v.begin(), v.end());
         reverse(v.begin(), v.end());
         for(int i=0;i<v.size();i++){
+            ans+= (target>v[i].second)? (v[i].first*v[i].second):(v[i].first*target);
             if(target>v[i].second){
                 target-=v[i].second;
-                ans+=(v[i].first*v[i].second);
+                // ans+=(v[i].first*v[i].second);
             }
             else{
-                ans+=(v[i].first*target);
+                // ans+=(v[i].first*target);
                 break;
             }
         }
-        cout<<sumB;
         return ans;
     }
 };
