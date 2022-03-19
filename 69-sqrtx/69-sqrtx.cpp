25ef72b000f13887b680284ea6a06e24121972d1
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-03-19 11:09:05 +0530

    Time: 40 ms (6.31%), Space: 6 MB (26.91%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -4,8 +4,7 @@ public:
         if(x==1){return 1;}
         long long int j = 1;
         while(j<=x/2){
-                        j++;
-
+            j++;
             if(j*j > x){return j-1;}
         }
         return 0;
