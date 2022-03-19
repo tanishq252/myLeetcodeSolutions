1a7679592d85a8d60f224f3d5596b5dc89869779
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-03-19 11:10:48 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (86.95%) - LeetHub

---
+++ 367-valid-perfect-square/367-valid-perfect-square.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x==1){return 1;}
+        long long int j = 1;
+        while(j<=x/2){
+            j++;
+            if(j*j > x){return j-1;}
+        }
+        return 0;
+    }
+    bool isPerfectSquare(int num) {
+        return mySqrt(num)*mySqrt(num) == num;
+    }
+};
