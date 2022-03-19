f425db48fab81c3246265b9e7f6a1b8378f1a0b1
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-03-19 11:08:47 +0530

    Time: 20 ms (17.33%), Space: 6 MB (26.91%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x==1){return 1;}
+        long long int j = 1;
+        while(j<=x/2){
+                        j++;
+
+            if(j*j > x){return j-1;}
+        }
+        return 0;
+    }
+};
