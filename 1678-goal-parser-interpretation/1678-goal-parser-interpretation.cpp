2db985eeaf10eb82ac33e26b66cbb485f601d63e
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-03-19 09:36:13 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (55.86%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string interpret(string command) {
+        string s;
+        for(int i=0;i<command.length();i++){
+            if(command[i] == 'G'){s+="G";}
+            else if(command[i] == '(' && command[i+1] == ')'){s+="o"; i++;}
+            else if(command[i] == '(' && command[i+1] == 'a'){s+="al"; i+=3;}
+        }
+        return s;
+    }
+};
