2545cc6bd7f43758396cd7bbd79eed0840fdf93f
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-03-19 21:54:16 +0530

    Time: 8 ms (7.16%), Space: 7.2 MB (6.49%) - LeetHub

---
+++ 2103-rings-and-rods/2103-rings-and-rods.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countPoints(string rings) {
+        map <int, set<char>> m;
+        set<char> s;
+        for(int i=0;i<rings.size();i+=2){
+            m[rings[i+1]].insert(rings[i]);
+        }
+        int c=0;
+        for(int i=0;i<m.size();i++){
+            c+= (m[i].size()==3)?1:0;
+        }
+        return c;
+    }
+};
