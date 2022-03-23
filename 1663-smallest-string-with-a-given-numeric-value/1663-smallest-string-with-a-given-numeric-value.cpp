3c4e1443bff85e89c674da13288330367ebea8ad
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-03-23 09:18:11 +0530

    Time: 55 ms (60.48%), Space: 21 MB (90.83%) - LeetHub

--- 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.cpp
+++ 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.cpp
@@ -4,15 +4,15 @@ public:
         // initialising string of length n with all 'a';
         string str(n,'a');
         
-        // as all 'a' are 1 and therefore we have subtract there sum;
         k -= n;
         
         while( k > 0)
         {
             // turning rightmost digit , 'a' into 'z' ('a' + 25, or 'a' + k)
             // while k is positive.
-            str[--n] += min(25,k);
-            k -= min(25,k);
+            int j = min(25, k);
+            str[--n] += j;
+            k -= j;
         }
         
         return str;
