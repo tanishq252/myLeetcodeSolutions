e5a23feac52df08507a6e738d27d1ec2c12aeb98
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-03-25 08:02:14 +0530

    Time: 6 ms (56.24%), Space: 8 MB (33.00%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     int twoCitySchedCost(vector<vector<int>>& costs) {
-        int sumA = 0;
-        vector<int> v;
-        for(auto i:costs){
-            sumA+=(i[0]);
-            v.push_back(i[1]-i[0]);
+        vector<pair<int, int>> v;
+        for(int i=0;i<costs.size();i++){
+            v.push_back(make_pair(costs[i][0]-costs[i][1], i ));
         }
         sort(v.begin(), v.end());
-        int s = accumulate(v.begin(), v.begin()+(v.size()/2), sumA);
+        int s=0;
+        for(int i=0;i<v.size()/2;i++){
+            s+=(costs[v[i].second][0]);
+        }
+        for(int i=v.size()/2;i<v.size();i++){
+            s+=(costs[v[i].second][1]);
+        }
         return s;
     }
 };
