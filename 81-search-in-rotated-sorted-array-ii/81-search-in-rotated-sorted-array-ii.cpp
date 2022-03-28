68764d13bd9b11061598727daac3f27dd8b1e121
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-03-28 14:53:08 +0530

    Time: 11 ms (41.14%), Space: 14 MB (32.08%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
@@ -1,6 +1,33 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        return (count(nums.begin(), nums.end(), target)>=1);
+        int n = nums.size();
+        if(n==1 && nums[0]!=target){return false;}
+        if(nums[0] == target||nums[nums.size()-1] == target){return true;}
+        if(target<nums[n-1]){
+            int i = n-1;
+            while(target<nums[i]&&i>=0){
+                i--;
+                if(target == nums[i]){
+                    return true;
+                }
+                if(i==0){
+                    return false;
+                }
+            }
+        }
+        else if(target>nums[0]){
+            int i = 0;
+            while(target>nums[i]&&i<=n-1){
+                i++;
+                if(target == nums[i]){
+                    return true;
+                }
+                if(i==n-1){
+                    return false;
+                }
+            }
+        }
+        return false;
     }
 };
