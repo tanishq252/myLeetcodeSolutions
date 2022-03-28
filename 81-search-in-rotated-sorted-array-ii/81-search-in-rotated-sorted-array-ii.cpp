81940c71f4a9f0e3899207e8aa543844b6bf90d2
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-03-28 15:57:12 +0530

    Time: 12 ms (32.08%), Space: 13.9 MB (71.85%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
@@ -4,27 +4,28 @@ public:
         int n = nums.size();
         if(n==1 && nums[0]!=target){return false;}
         if(nums[0] == target||nums[nums.size()-1] == target){return true;}
-        if(target<nums[n-1]){
-            int i = n-1;
-            while(target<nums[i]&&i>=0){
-                i--;
-                if(target == nums[i]){
-                    return true;
-                }
-                if(i==0){
-                    return false;
+        int low = 0;
+        int high = n-1;
+        while (low< high && nums[low] == nums[low + 1])
+            ++low;
+        while (low< high && nums[high] == nums[high - 1])
+            --high;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(nums[mid] == target){return true;}
+            
+            if(nums[mid]>=nums[low]){
+                if(target<nums[mid]&&target>=nums[low]){
+                    high =mid-1;
+                }else{
+                    low = mid+1;
                 }
             }
-        }
-        else if(target>nums[0]){
-            int i = 0;
-            while(target>nums[i]&&i<=n-1){
-                i++;
-                if(target == nums[i]){
-                    return true;
-                }
-                if(i==n-1){
-                    return false;
+            else{
+                if(target>nums[mid]&&target<=nums[high]){
+                    low = mid+1;
+                }else{
+                    high = mid-1;
                 }
             }
         }
