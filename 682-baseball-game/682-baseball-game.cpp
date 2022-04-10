94e426c953bc4338ab8e16d6fa5672d840105274
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-04-10 19:24:38 +0530

    Time: 8 ms (30.74%), Space: 8.6 MB (28.60%) - LeetHub

--- 682-baseball-game/682-baseball-game.cpp
+++ 682-baseball-game/682-baseball-game.cpp
@@ -2,14 +2,32 @@ class Solution {
 public:
     int calPoints(vector<string>& ops) {
         vector<int> v;
+        stack<int> st;
         for(int i=0;i<ops.size();i++){
-            if(ops[i]=="C"){v.pop_back();}
-            else if(ops[i]=="D"){v.push_back(v[v.size()-1]*2);}
-            else if(ops[i]=="+"){v.push_back(v[v.size()-1]+v[v.size()-2]);}
-            else{v.push_back(stoi(ops[i]));}
+            if(ops[i]=="C"){
+                st.pop();
+                v.pop_back();}
+            else if(ops[i]=="D"){
+                st.push(st.top()*2);
+                v.push_back(v[v.size()-1]*2);}
+            else if(ops[i]=="+"){
+                int prev = st.top();
+                st.pop();
+                int newScore = prev+st.top();
+                st.push(prev);
+                st.push(newScore);
+                v.push_back(v[v.size()-1]+v[v.size()-2]);}
+            else{
+                st.push(stoi(ops[i]));
+                v.push_back(stoi(ops[i]));}
         }
         int s = 0;
-        for(auto i:v){s+=i;}
+        int sum;
+        while(!st.empty()){
+            cout<<st.top()<<" ";
+            s+=st.top();
+            st.pop();
+        }
         return s;
     }
 };
