e7b43913b471b37167f730279da1696349b39115
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-04-11 19:48:54 +0530

    Time: 24 ms (89.17%), Space: 18.4 MB (6.27%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -1,20 +1,5 @@
 class Solution {
 public:
-    
-    void rotateArray(vector<int> &v, int k){
-        if(k==0){
-            return;
-        }else{
-            int n = v.size();
-            int t = v[n-1];
-            for(int i=n-1;i>0;i--){
-                v[i] = v[i-1];
-            }
-            v[0] = t;
-        }
-        rotateArray(v, k-1);
-    }
-    
     vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
         vector<int> v;
         int n = grid.size();
@@ -24,17 +9,24 @@ public:
                 v.push_back(j);
             }
         }
-        rotateArray(v, k);
+        vector<int> rotatedA;
+        int nn = n*m;
+        k%=v.size();
+        for(int i=nn-k;i<nn;i++){
+            rotatedA.push_back(v[i]);
+        }
+        for(int i=0;i<nn-k;i++){
+            rotatedA.push_back(v[i]);
+        }
         vector<vector<int>> ans;
         int l = 0;
         for(int i=0;i<n;i++){
             vector<int> t;
             for(int j=0;j<m;j++){
-                t.push_back(v[l++]);
+                t.push_back(rotatedA[l++]);
             }
             ans.push_back(t);
         }
         return ans;
-        
     }
 };
