c3a7c8e284fed03bf60a281d89aa5c1bc0e59237
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-04-12 20:27:36 +0530

    Time: 7 ms (9.45%), Space: 7.1 MB (20.27%) - LeetHub

--- 289-game-of-life/289-game-of-life.cpp
+++ 289-game-of-life/289-game-of-life.cpp
@@ -7,46 +7,21 @@ public:
         vector<int> neighbors;
         if(i-1>=0&&j-1>=0&&i-1<=n&&j-1<=m){neighbors.push_back(board[i-1][j-1]);}        
         if(i-1>=0&&j+1>=0&&i-1<=n&&j+1<=m){neighbors.push_back(board[i-1][j+1]);}        
-         if(i-1>=0&&j>=0&&i-1<=n&&j<=m){neighbors.push_back(board[i-1][j]);}
+        if(i-1>=0&&j>=0&&i-1<=n&&j<=m){neighbors.push_back(board[i-1][j]);}
         if(i+1>=0&&j-1>=0&&i+1<=n&&j-1<=m){neighbors.push_back(board[i+1][j-1]);}        
         if(i+1>=0&&j+1>=0&&i+1<=n&&j+1<=m){neighbors.push_back(board[i+1][j+1]);}        
-         if(i+1>=0&&j>=0&&i+1<=n&&j<=m){neighbors.push_back(board[i+1][j]);}
+        if(i+1>=0&&j>=0&&i+1<=n&&j<=m){neighbors.push_back(board[i+1][j]);}
         if(i>=0&&j-1>=0&&i<=n&&j-1<=m){neighbors.push_back(board[i][j-1]);}        
         if(i>=0&&j+1>=0&&i<=n&&j+1<=m){neighbors.push_back(board[i][j+1]);}        
-        
-        int c1 = count(neighbors.begin(),neighbors.end(),1);        
-        int c0 = count(neighbors.begin(),neighbors.end(),0);
-        
-        if(board[i][j]==1){
-            if(c1 < 2){
-                dumboard[i][j] = 0;
-            }else if(c1==2 || c1==3){
-                dumboard[i][j] = 1;
-            }
-            else if(c1>3){dumboard[i][j] = 0;}
-        }else if(board[i][j] == 0){
-            if(c1==3){
-                dumboard[i][j] = 1;
-            }
-        }
-        
-        cout<<c1<<" ";
-
+        int c1 = count(neighbors.begin(),neighbors.end(),1);  
+        dumboard[i][j] = (board[i][j]==1)?((c1==2 || c1==3)?1:0):(c1==3)?1:0;
     }
     
     void gameOfLife(vector<vector<int>>& board) {
         int n = board.size();
         int m = board[0].size();
         
-        vector<vector<int>> dumboard;
-        
-        for(int i=0;i<n;i++){
-            vector<int> v;
-            for(int j=0;j<m;j++){
-                v.push_back(0);
-            }
-            dumboard.push_back(v);
-        }
+        vector<vector<int>> dumboard(n, vector<int>(m,0));
         
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
