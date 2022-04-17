56012b87df2f2573f250ca54ef738dbbc01a82fd
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-04-17 21:34:47 +0530

    Time: 9 ms (6.64%), Space: 8.2 MB (6.25%) - LeetHub

---
+++ 897-increasing-order-search-tree/897-increasing-order-search-tree.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void inorder(vector<int> &v, TreeNode* root){
+        if(root==NULL){
+            return;
+        }
+        inorder(v, root->left);
+        v.push_back(root->val);
+        inorder(v, root->right);
+        
+    }
+    
+    TreeNode* formTree(vector<int> v){
+        TreeNode *newTree = new TreeNode(v[0]);
+        TreeNode *ptr = newTree;
+        if(v.size()>1){
+            for(int i=1;i<v.size();i++){
+                TreeNode *newNode = new TreeNode(v[i]);
+                ptr->left = NULL;
+                ptr->right = newNode;
+                ptr = ptr->right;
+            }
+        }
+        return newTree;
+    }
+    
+    TreeNode* increasingBST(TreeNode* root) {
+        vector<int> v;
+        inorder(v, root);
+        for(auto i:v){cout<<i<<" ";}
+        TreeNode *newTree = formTree(v);
+        return newTree;
+        
+    }
+};
