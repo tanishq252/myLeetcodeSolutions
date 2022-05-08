407aa84ebd6fc2c01b2113af5bd61eeea3d9c5e4
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-05-08 22:15:56 +0530

    Time: 408 ms (16.13%), Space: 49.5 MB (11.20%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -15,9 +15,6 @@ public:
                 int k = j+1;
                 int l = n-1;
                 while(k<l){
-                    // if(target<nums[i]||target<nums[j]||target<nums[k]||target<nums[l]){
-                    //     break;
-                    // }
                     int s = ((nums[i]%1000000007+nums[j]%1000000007)%1000000007+(nums[k]%1000000007+nums[l]%1000000007)%1000000007)%1000000007;
                     if(s==target){
                         vector<int> v = {nums[i], nums[j], nums[k], nums[l]};
