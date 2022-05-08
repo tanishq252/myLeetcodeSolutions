270246520633fec0471cfa42e3dcd05dd300fbcb
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-05-08 10:30:44 +0530

    Time: 145 ms (29.91%), Space: 54.9 MB (6.40%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        if(nums.size() == 1){return false;}
-        sort(nums.begin(), nums.end());
-        for(int i=1;i<nums.size();i++){
-            if(nums[i] == nums[i-1]){return true;}
+        set<int> s;
+        for(auto i:nums){
+            s.insert(i);
         }
-        return false;
+        return s.size() != nums.size();
     }
 };
