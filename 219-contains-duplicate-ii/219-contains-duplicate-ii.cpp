8f7258ea19ef280b3f5438af022fefae30e39ddc
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-05-08 10:12:45 +0530

    Time: 362 ms (13.46%), Space: 106.6 MB (5.01%) - LeetHub

--- 219-contains-duplicate-ii/219-contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/219-contains-duplicate-ii.cpp
@@ -2,9 +2,12 @@ class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         vector<pair<long long int, int>> v;
+        set<int> s;
         for(int i=0;i<nums.size();i++){
             v.push_back({nums[i],i});
+            s.insert(nums[i]);
         }
+        if(s.size()==v.size()){return false;}
         sort(v.begin(), v.end());
         for(int i=0;i<v.size();i++){
             for(int j=i+1;j<v.size();j++){
