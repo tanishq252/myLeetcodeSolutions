7ccc431b4402a44a10a6cf401c73bda22278f1e2
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-05-08 20:36:59 +0530

    Time: 163 ms (35.37%), Space: 27.9 MB (15.17%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -19,8 +19,9 @@ public:
                     int f = nums[i]+nums[j]+nums[k];
                     if(f==0){
                         if(i!=j && i!=k)
-                            {vector<int> v = {nums[i], nums[j], nums[k]};
-                            s.insert(v);
+                            {
+                                vector<int> v = {nums[i], nums[j], nums[k]};
+                                s.insert(v);
                             }
                         j++; 
                         k--;
@@ -29,9 +30,6 @@ public:
                     }else if(f>0){
                         k--;
                     }
-                if(j==nums.size() || k==0){
-                    break;
-                }
             }
         }
         for(auto i:s){
