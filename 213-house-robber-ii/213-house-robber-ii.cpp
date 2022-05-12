2d789f6a2f6e6163f172779008c944e27b376e1b
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-05-12 11:36:36 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (60.91%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -5,53 +5,28 @@ public:
             return nums[0];
         }
         int odd = 0, even = 0;
-        // if(nums[0]>nums[nums.size()-1]){
-        //     for(int i=0;i<nums.size()-1;i++){
-        //         if(i%2==0){
-        //             even+=nums[i];
-        //             even = max(even, odd);
-        //         }else{
-        //             odd+=nums[i];
-        //             odd = max(even, odd);
-        //         }
-        //     }
-        // }
-        // else if(nums[0]==nums[nums.size()-1]){
-            for(int i=0;i<nums.size()-1;i++){
-                if(i%2==0){
-                    even+=nums[i];
-                    even = max(even, odd);
-                }else{
-                    odd+=nums[i];
-                    odd = max(even, odd);
-                }
+        for(int i=0;i<nums.size()-1;i++){
+            if(i%2==0){
+                even+=nums[i];
+                even = max(even, odd);
+            }else{
+                odd+=nums[i];
+                odd = max(even, odd);
             }
-            int mx = max(even, odd);
-            even =0;
-            odd=0;
-            for(int i=1;i<nums.size();i++){
-                if(i%2==0){
-                    even+=nums[i];
-                    even = max(even, odd);
-                }else{
-                    odd+=nums[i];
-                    odd = max(even, odd);
-                }
+        }
+        int mx = max(even, odd);
+        even =0;
+        odd=0;
+        for(int i=1;i<nums.size();i++){
+            if(i%2==0){
+                even+=nums[i];
+                even = max(even, odd);
+            }else{
+                odd+=nums[i];
+                odd = max(even, odd);
             }
-            int mx1 = max(even, odd);
-            return max(mx, mx1);
-        // }
-        // else{
-        //     for(int i=1;i<nums.size();i++){
-        //         if(i%2==0){
-        //             even+=nums[i];
-        //             even = max(even, odd);
-        //         }else{
-        //             odd+=nums[i];
-        //             odd = max(even, odd);
-        //         }
-        //     }
-        // }
-        // return max(even, odd);
+        }
+        int mx1 = max(even, odd);
+        return max(mx, mx1);
     }
 };
