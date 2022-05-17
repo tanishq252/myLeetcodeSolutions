1d33e77675bd5ef01710a655194492d195ea02c0
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-05-17 12:01:45 +0530

    Time: 73 ms (5.04%), Space: 24 MB (13.17%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -9,16 +9,7 @@
  */
 class Solution {
 public:
-    TreeNode *roott;
-    // void inOrder(TreeNode *root, TreeNode* p,TreeNode* q, vector<pair<int, TreeNode*>> &ancestors){
-    //     if(root==NULL){return;}
-    //     inOrder(root->left, p, q, ancestors);        
-    //     if(root->val >= p->val and root->val <= q->val and nodedepth(roott, p, 0)>=nodedepth(roott, root, 0) and nodedepth(roott, q, 0)>=nodedepth(roott, roott, 0)){
-    //         ancestors.push_back({nodedepth(roott, root, 0),root});
-    //     }
-    //     inOrder(root->right, p, q, ancestors);
-    // }
-    
+
     void inOrder(TreeNode *root, TreeNode* p,TreeNode* q, vector<pair<int, TreeNode*>> &ancestors, int depth){
         if(root==NULL){return;}
         inOrder(root->left, p, q, ancestors, depth+1);        
@@ -27,26 +18,12 @@ public:
         }
         inOrder(root->right, p, q, ancestors, depth+1);
     }
-    
-    int nodedepth(TreeNode *root, TreeNode *node,int depth){
-        if(root == node || root==NULL) return 0;
-        if(root->val > node->val) {
-            return nodedepth(root->left, node, depth)+1;}
-        else {
-            return nodedepth(root->right, node, depth)+1;}
-        return 0;
-    }
-    
-    void InOrder(TreeNode *root, TreeNode* p,TreeNode* q, int depth){
-        
-    }
-    
+
     bool findDescedant(){
         return false;
     }
     
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        roott = root;
         vector<pair<int, TreeNode*>> ancestors;
         TreeNode* small;
         TreeNode *large;
