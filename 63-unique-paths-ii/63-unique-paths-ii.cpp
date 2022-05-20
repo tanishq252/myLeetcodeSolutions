74983c0d36e0a092ec4fcb58e3358fbc36a4f42c
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-05-20 13:15:22 +0530

    Time: 3 ms (80.74%), Space: 7.8 MB (60.56%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -5,22 +5,36 @@ public:
         int m=obstacleGrid.size();
         int n=obstacleGrid[0].size();
         if(obstacleGrid[0][0]==1) return 0;
-        obstacleGrid[0][0]=1;
-        for(int i=1;i<m;i++){
-            obstacleGrid[i][0] = (obstacleGrid[i][0]==0 and obstacleGrid[i-1][0]==1) ? 1:0;
-        }
-        for(int i=1;i<n;i++){
-            obstacleGrid[0][i] = (obstacleGrid[0][i]==0 and obstacleGrid[0][i-1]==1) ? 1:0;
-        }
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                if(obstacleGrid[i][j]==0){
-                    obstacleGrid[i][j] = obstacleGrid[i-1][j]+obstacleGrid[i][j-1];
+        vector<vector<int>> dp(m+1, vector<int> (n+1, 0));
+        dp[0][1] =1;
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(obstacleGrid[i-1][j-1]!=1){
+                    dp[i][j] = dp[i][j-1]+dp[i-1][j];
                 }else{
-                    obstacleGrid[i][j]=0;
+                    dp[i][j] = 0;
                 }
             }
         }
-        return obstacleGrid[m-1][n-1];
+        
+        return dp[m][n];
+        
+        // obstacleGrid[0][0]=1;
+        // for(int i=1;i<m;i++){
+        //     obstacleGrid[i][0] = (obstacleGrid[i][0]==0 and obstacleGrid[i-1][0]==1) ? 1:0;
+        // }
+        // for(int i=1;i<n;i++){
+        //     obstacleGrid[0][i] = (obstacleGrid[0][i]==0 and obstacleGrid[0][i-1]==1) ? 1:0;
+        // }
+        // for(int i=1;i<m;i++){
+        //     for(int j=1;j<n;j++){
+        //         if(obstacleGrid[i][j]==0){
+        //             obstacleGrid[i][j] = obstacleGrid[i-1][j]+obstacleGrid[i][j-1];
+        //         }else{
+        //             obstacleGrid[i][j]=0;
+        //         }
+        //     }
+        // }
+        // return obstacleGrid[m-1][n-1];
     }
 };
