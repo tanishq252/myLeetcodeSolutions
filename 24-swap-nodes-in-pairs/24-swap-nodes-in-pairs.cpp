c7f862cd3f6d68e9ae0a6ac60480b8e8e1f2a979
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-05-26 12:35:02 +0530

    Time: 11 ms (6.04%), Space: 7.5 MB (91.97%) - LeetHub

--- 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cpp
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cpp
@@ -10,29 +10,39 @@
  */
 class Solution {
 public: 
-    void insert(ListNode **head, int data){
-        if((*head) == NULL) {(*head) = new ListNode(data);return;}
-        ListNode *temp = *head;
-        while(temp->next!=NULL){
-            temp = temp->next;
-        }
-        temp->next = new ListNode(data);
-    }
+    
     ListNode* swapPairs(ListNode* head) {
         if(head ==NULL or head->next==NULL) return head;
-        ListNode *temp = head;
-        ListNode *headnew = NULL;
-        while(temp->next!=NULL and temp!=NULL){
-            insert(&headnew, temp->next->val);
-            insert(&headnew, temp->val);
-            if(temp->next->next!=NULL){
-                if( temp->next->next->next ==NULL ){
-                    insert(&headnew, temp->next->next->val);
-                }
-            }
-            temp = temp->next->next;
-            if(temp==NULL) break;
-        }
-        return head=headnew;
+        ListNode *t = head->next;
+        head->next = swapPairs(head->next->next);
+        t->next = head;
+        return t;
+ 
     }
+    
+    // void insert(ListNode **head, int data){
+    //     if((*head) == NULL) {(*head) = new ListNode(data);return;}
+    //     ListNode *temp = *head;
+    //     while(temp->next!=NULL){
+    //         temp = temp->next;
+    //     }
+    //     temp->next = new ListNode(data);
+    // }
+    // ListNode* swapPairs(ListNode* head) {
+    //     if(head ==NULL or head->next==NULL) return head;
+    //     ListNode *temp = head;
+    //     ListNode *headnew = NULL;
+    //     while(temp->next!=NULL and temp!=NULL){
+    //         insert(&headnew, temp->next->val);
+    //         insert(&headnew, temp->val);
+    //         if(temp->next->next!=NULL){
+    //             if( temp->next->next->next ==NULL ){
+    //                 insert(&headnew, temp->next->next->val);
+    //             }
+    //         }
+    //         temp = temp->next->next;
+    //         if(temp==NULL) break;
+    //     }
+    //     return head=headnew;
+    // }
 };
