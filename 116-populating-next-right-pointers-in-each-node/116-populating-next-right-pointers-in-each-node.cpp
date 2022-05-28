099461a11aaa33df413c620f32b82ba7fab55fb3
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-05-28 11:22:10 +0530

    Time: 38 ms (16.01%), Space: 18 MB (5.85%) - LeetHub

---
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cpp
@@ -0,0 +1,41 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    void leveltraverse(Node *root, vector<vector<Node*>> &level,int depth){
+        if(root==NULL) return;
+        if(depth == level.size()){
+            level.push_back({});
+        }
+        level[depth].push_back(root);
+        leveltraverse(root->left, level, depth+1);        
+        leveltraverse(root->right, level, depth+1);
+    }
+    
+    Node* connect(Node* root) {
+        vector<vector<Node*>> level;
+        leveltraverse(root, level, 0);
+        for(auto i:level){
+            for(int j=0;j<i.size()-1;j++){
+                i[j]->next = i[j+1];
+            }
+        }
+        return root;
+    }
+};
