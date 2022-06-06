be672b880ed972e8ae962be4ec4bc005e9b47754
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-06-06 16:46:09 +0530

    Time: 519 ms (5.10%), Space: 144.7 MB (43.76%) - LeetHub

--- 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.cpp
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.cpp
@@ -11,12 +11,21 @@
  */
 class Solution {
 public:
+    int mindepth = 0;
+    
+    void levelOrder(TreeNode *root,int depth){
+        if(root==NULL) {mindepth = depth-1; return;}
+        levelOrder(root->left, depth+1);
+        levelOrder(root->right, depth+1);
+    }
+    
     int minDepth(TreeNode* root) {
         if(root==NULL) return 0;
         int l = minDepth(root->left);
         int r = minDepth(root->right);
         if(root->left == NULL) return r+1;
-        else if(root->right == NULL) return l+1;
+        if(root->right==NULL) return l+1;
         return min(l,r)+1;
+        
     }
 };
