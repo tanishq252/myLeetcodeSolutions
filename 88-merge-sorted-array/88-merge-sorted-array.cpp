def071078d11114ad32ed9b14297f5672a036721
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-06-07 07:32:58 +0530

    Time: 0 ms (100.00%), Space: 9 MB (90.29%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,16 +1,26 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = m - 1, j = n - 1, k = m + n - 1;
-	while (j >= 0) {
-		if (i < 0 || nums2[j] >= nums1[i]) {
-			nums1[k] = nums2[j];
-			j--;
-		} else {
-			nums1[k] = nums1[i];
-			i--;
-		}
-		k--;
-	}
+        if(m == 0) {
+            nums1=nums2;
+            return;
+        }
+        else if(n==0) return;
+        int i=m-1, j = n-1, k = m+n-1;
+        while(i>=0 and j>=0){
+            if(nums1[i]>nums2[j]){
+                nums1[k] = nums1[i];
+                i--;
+            }
+            else if(nums1[i]<=nums2[j]){
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+
+        while(j>=0){
+            nums1[k] = nums2[j];j--;k--;
+        }
     }
 };
