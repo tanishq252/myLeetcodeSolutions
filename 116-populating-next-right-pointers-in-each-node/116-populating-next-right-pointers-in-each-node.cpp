16338d77b0c32ddddf9d69025af9bf9342ff82b9
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-06-08 20:46:47 +0530

    Time: 34 ms (25.41%), Space: 17.7 MB (6.16%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cpp
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cpp
@@ -18,48 +18,25 @@ public:
 
 class Solution {
 public:
-//     void leveltraverse(Node *root, vector<vector<Node*>> &level,int depth){
-//         if(root==NULL) return;
-//         if(depth == level.size()){
-//             level.push_back({});
-//         }
-//         level[depth].push_back(root);
-//         leveltraverse(root->left, level, depth+1);        
-//         leveltraverse(root->right, level, depth+1);
-//     }
-    
-//     Node* connect(Node* root) {
-//         vector<vector<Node*>> level;
-//         leveltraverse(root, level, 0);
-//         for(auto i:level){
-//             for(int j=0;j<i.size()-1;j++){
-//                 i[j]->next = i[j+1];
-//             }
-//         }
-//         return root;
-//     }
-    
-    void join(Node* root){
-        if(!root){
-            return;
+    void lvlOrder(Node *root, vector<vector<Node*>> &lvl, int depth){
+        if(root==NULL) return;
+        if(lvl.size() == depth){
+            lvl.push_back({});
         }
-        
-        if(!root->left and !root->right){
-            return;
-        }
-        
-        root->left->next = root->right;
-        
-        if(root->next){
-            root->right->next = root->next->left;
-        }
-        
-        join(root->left);
-        join(root->right);
+        lvlOrder(root->left, lvl, depth+1);
+        lvl[depth].push_back(root);
+        lvlOrder(root->right, lvl, depth+1);
+
     }
     
     Node* connect(Node* root) {
-        join(root);
+        vector<vector<Node*>> lvl;
+        lvlOrder(root, lvl, 0);
+        for(auto i:lvl){
+            for(int j=0;j<i.size()-1;j++){
+                i[j]->next = i[j+1];
+            }
+        }
         return root;
     }
 };
