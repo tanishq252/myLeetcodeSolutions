db7136789a9bacd392e04eeb484411be42737576
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-06-16 18:51:56 +0530

    Time: 7 ms (55.31%), Space: 6.3 MB (53.92%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     void solve(vector<int> &v, int ind, int k){
+        if(v.size() == 1){
+            return;
+        }
         ind = (ind+k)%v.size();
-        if(v.size()==1){return;}
         v.erase(v.begin()+ind);
         solve(v, ind, k);
     }
     
     int findTheWinner(int n, int k) {
-        vector<int> v;
-        int ans=0, ind = 0;
-        for(int i=0;i<n;i++){
-            v.push_back(i+1);
-        }
-        solve(v, ind, k-1);
+        vector<int>v (n, 0);
+        for(int i=0;i<n;i++) v[i] = i+1;
+        solve(v, 0, k-1);
         return v[0];
     }
 };
