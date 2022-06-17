287c8435694ea224f0164baf0fdc6d27f0e6fcff
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-06-17 23:46:11 +0530

    Time: 7 ms (96.05%), Space: 21.1 MB (98.60%) - LeetHub

---
+++ 968-binary-tree-cameras/968-binary-tree-cameras.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minCameraCover(TreeNode* root) {
+        int noOfCam = 0;
+        int ans = helper(root, noOfCam);
+        
+        if(ans == -1)   //we need to fix a camera
+            noOfCam++;
+        
+        return noOfCam;
+    }
+    
+    int helper(TreeNode* root, int &noOfCam){
+        if(root== NULL) //I am not even a node, Tell them I dont need cam at all
+            return 1;
+        
+        int left = helper(root->left, noOfCam);
+        int right = helper(root->right, noOfCam);
+        
+        //child wants camera, install 1 cam there
+        if(left==-1 || right==-1){
+            noOfCam++;
+            return 0;   //tell cam is installed here
+        }
+        
+        //if at any node cam is installed, tell them we dont need cam here
+        if(left==0 || right==0)
+            return 1;
+        
+        //1-> this node is not covered go and tell parent please install a cam
+        return -1;
+        
+    }
+};
