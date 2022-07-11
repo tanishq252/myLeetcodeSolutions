c639d2c507fe3b653c72f20dee7a14b48d1d278b
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-07-11 09:01:01 +0530

    Time: 16 ms (5.32%), Space: 13.2 MB (6.59%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -11,20 +11,23 @@
  */
 class Solution {
 public:
-    void leveltraverse(TreeNode *root, vector<vector<int>> &level,int depth){
-        if(root==NULL) return;
-        if(depth == level.size()){
-            level.push_back({});
+    void levelOrder(vector<vector<int>> &lvl, TreeNode *root, int level){
+        if(root==NULL){
+            return;
         }
-        level[depth].push_back(root->val);
-        leveltraverse(root->left, level, depth+1);        
-        leveltraverse(root->right, level, depth+1);
+        if(lvl.size() == level){
+            lvl.push_back({});
+        }
+        lvl[level].push_back(root->val);
+        levelOrder(lvl, root->left, level+1);        
+        levelOrder(lvl, root->right, level+1);
+
     }
     vector<int> rightSideView(TreeNode* root) {
+        vector<vector<int>> lvl;
         vector<int> ans;
-        vector<vector<int>> level;
-        leveltraverse(root, level, 0);
-        for(auto i:level){
+        levelOrder(lvl, root, 0);
+        for(auto i:lvl){
             ans.push_back(i[i.size()-1]);
         }
         return ans;
