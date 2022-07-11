e00e3093b0d90cc9d9ca19d64ff2d5d303686c1d
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-07-11 12:21:19 +0530

    Time: 157 ms (18.56%), Space: 34.3 MB (35.19%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
@@ -1,16 +1,27 @@
 class Solution {
 public:
     int maxSumDivThree(vector<int>& nums) {
-         vector<vector<long long>> dp(nums.size()+1, vector<long long>(3, INT_MIN));
-        long long a, b;
-        dp[0][0] = 0;
+         return maxSumDivK(nums, 3);
+    }
+    
+    int maxSumDivK(vector<int> nums, int k){
+        vector<int> dp(k, INT_MIN);
+        dp[0] = 0;
+        
         for(int i=1;i<=nums.size();i++){
-            for(int j=0;j<=2;j++){
-                a = nums[i-1] + dp[i-1][(j+nums[i-1])%3];
-                b = dp[i-1][j];
-                dp[i][j] = max(a, b);
+            int n = nums[i-1];
+            int mod = nums[i-1]%k;
+            
+            //create a temporary array
+            vector<int> newDP(k, 0);
+            
+            //iterate through all the columns
+            for(int j=0; j<k; j++){
+                newDP[j] = max(dp[j], n+dp[(j+mod)%k]);
             }
+            dp = newDP;
         }
-        return dp[nums.size()][0];
+        
+        return dp[0];
     }
 };
