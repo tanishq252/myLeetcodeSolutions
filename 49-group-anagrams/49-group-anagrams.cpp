9d3fbd5296df670eab4b326ef2ffa1bbf4213d4a
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-07-19 15:14:45 +0530

    Time: 66 ms (39.89%), Space: 20.8 MB (39.97%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -3,7 +3,6 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         string ans;
         map<string, vector<string>> mp;
-        set<vector<string>> ansSet;
         for(auto i: strs){
             ans = i;
             sort(ans.begin(), ans.end());
@@ -13,7 +12,6 @@ public:
         for(auto i: mp){
             ansVec.push_back(i.second);
         }
-        // vector<vector<string>> ansVec(ansSet.begin(), ansSet.end());
         return ansVec;
     }
 };
