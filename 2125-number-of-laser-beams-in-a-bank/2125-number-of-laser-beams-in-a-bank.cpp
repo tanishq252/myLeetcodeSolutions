7cd81f71643aaae2d75b9016e9650944db9ab0ec
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-07-23 11:14:33 +0530

    Time: 163 ms (55.02%), Space: 23.3 MB (34.04%) - LeetHub

---
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numberOfBeams(vector<string>& bank) {
+        if(bank.size() == 1) return 0;
+        vector<int> beams;
+        for(int i = 0;i<bank.size();i++){
+            int c = count(bank[i].begin(), bank[i].end(), '1');
+            if(c){
+                beams.push_back(c);
+            }
+        }
+        int ans;
+        if(beams.size()){
+            for(int i=0;i<beams.size() - 1;i++){
+                beams[i]*=beams[i+1];
+                // ans+=beams[i];
+                cout<<beams[i]<<" ";
+            }
+            return accumulate(beams.begin(), beams.end()-1, 0);
+        }
+        return 0;
+    }
+};
