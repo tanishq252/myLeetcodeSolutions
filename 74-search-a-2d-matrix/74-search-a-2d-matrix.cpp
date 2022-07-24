678e9569e0c8a23af5c53a416743e686c6bb488f
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-07-24 08:44:53 +0530

    Time: 7 ms (53.15%), Space: 9.6 MB (51.72%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int row =0;
-        int j=matrix[0].size()-1;
-        for(int i=0;i<matrix.size();i++){
-            // row = (target <= *max_element(matrix[i].begin(), matrix[i].end())) ? i : -1 ;
-            // if(row!=-1){break;}
-            row = (matrix[i][j]>=target) ? i: -1;
-            if(row!=-1){break;}
+        int m=matrix.size();
+        int n=matrix[0].size();
+        
+        for(auto i:matrix){
+            if(count(i.begin(), i.end(), target)>=1){
+                return true;
+            }
         }
-        cout<<row;
-        if(row==-1){return false;}
-        int i=0;
-        j=matrix[0].size()-1;
-        while(i<=j){
-            if(matrix[row][i] == target || matrix[row][j] == target){return true;}
-            else if(matrix[row][i] < target){i++;}
-            else if(matrix[row][j] > target){j--;}
-        }
+        
         return false;
     }
 };
