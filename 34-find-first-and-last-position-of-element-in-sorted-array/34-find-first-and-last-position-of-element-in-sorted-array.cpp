c5108fa947413043609b3db1f52d0b9e90eaebb4
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-07-25 08:14:53 +0530

    Time: 11 ms (68.40%), Space: 13.8 MB (17.99%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,19 +1,30 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        if(nums.size() == 0) return {-1, -1};
-        vector<int> ans =  {-1, -1};
-        int i=0,j = nums.size()-1;
-        while(nums[i]<target ){
-            i++;
-            if(i==nums.size()){return {-1,-1};}
+        int i=0, j = nums.size()-1;
+        vector<int> ans;
+        while(i <= j){
+            if(nums[i]<target){
+                i++;
+            }
+            else if(nums[i] == target){
+                ans.push_back(i);
+                i++;
+            }
+            if(nums[j]>target){
+                j--;
+            }
+            else if(nums[j] == target){
+                ans.push_back(j);
+                j--;
+            }
         }
-        while(nums[j]>target and j>=0){
-            j--;
-            if(j<0){break;}
+        sort(ans.begin(), ans.end());
+        if(ans.size() == 1){
+            return {ans[0], ans[0]};
+        }else if(ans.size()>1){
+            return {ans[0], ans[ans.size()-1]};
         }
-        if(j<0 or i>nums.size()-1) return {-1, -1};
-        if(nums[i]!=target and nums[j]!=target){return {-1, -1};}
-        return {i, j};
+        return {-1, -1};
     }
 };
