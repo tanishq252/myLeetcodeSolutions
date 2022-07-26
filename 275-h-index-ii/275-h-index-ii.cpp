ee893edbbf302e9750bd8fde767b9c16ca7d89eb
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-07-26 20:53:00 +0530

    Time: 23 ms (73.87%), Space: 18.5 MB (99.38%) - LeetHub

--- 275-h-index-ii/275-h-index-ii.cpp
+++ 275-h-index-ii/275-h-index-ii.cpp
@@ -6,22 +6,20 @@ public:
         priority_queue<int> pq;
         sort(citations.begin(), citations.end());
         for(int i=0;i<=citations[n-1];i++){
+            
             //lower bound of current value 
             int lb = lower_bound(citations.begin(), citations.end(), i)-citations.begin();
-            //considering either the current value or the papers after current value
-            // if(i == n-lb or i==n-lb-1){
-            //     pq.push(i);         
-            // }
+            
             //upper bound of current value 
             int ub = upper_bound(citations.begin(), citations.end(), i)-citations.begin();
-            // if(i == n-ub){
-            //     pq.push(i);         
-            // }
-            //considering the values between lower bound and upper found
+
+            //considering the values between lower bound and upper found\
+            //considering either the current value or the papers after current value
+
             for(int j=n-ub;j<=n-lb;j++){
                 if(i == j){
-                pq.push(i);         
-            }
+                    pq.push(i);         
+                }
             }
         }
         return (!pq.empty()) ? pq.top() : (citations[0] ==0) ? citations[n-1] : citations[0];
