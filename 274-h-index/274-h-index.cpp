fda6882388d236130cf5a188064dbc74e0a36207
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-07-26 20:53:23 +0530

    Time: 8 ms (24.10%), Space: 8.5 MB (80.49%) - LeetHub

--- 274-h-index/274-h-index.cpp
+++ 274-h-index/274-h-index.cpp
@@ -2,31 +2,26 @@ class Solution {
 public:
     int hIndex(vector<int>& citations) {
         int n = citations.size();
+        //priority queue to store max H-index
         priority_queue<int> pq;
         sort(citations.begin(), citations.end());
-        int ind = lower_bound(citations.begin(), citations.end(), 2)-citations.begin();
         for(int i=0;i<=citations[n-1];i++){
+            
+            //lower bound of current value 
             int lb = lower_bound(citations.begin(), citations.end(), i)-citations.begin();
-            if(i == n-lb or i==n-lb-1){
-                pq.push(i);         
-                // break;
-                }
+            
+            //upper bound of current value 
             int ub = upper_bound(citations.begin(), citations.end(), i)-citations.begin();
-            cout<<n-lb<<" "<<i<<"\n";
-            if(i == n-ub){
-                pq.push(i);         
-                // break;
-            }
+
+            //considering the values between lower bound and upper found\
+            //considering either the current value or the papers after current value
+
             for(int j=n-ub;j<=n-lb;j++){
                 if(i == j){
-                pq.push(i);         
-                // break;
+                    pq.push(i);         
+                }
             }
-            }
-            // cout<<i<<" "<<lb<<"\n";
         }
-        // cout<<pq.top();
         return (!pq.empty()) ? pq.top() : (citations[0] ==0) ? citations[n-1] : citations[0];
-        // return 0;
     }
 };
