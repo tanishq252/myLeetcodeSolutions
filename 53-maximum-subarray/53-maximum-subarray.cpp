68a53f8ced80b1c38ced6f52452ced2bc059c367
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-07-26 12:12:16 +0530

    Time: 186 ms (35.72%), Space: 67.7 MB (53.44%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int sum = 0;
         int max = INT_MIN;
+        int sum=0;
         for(int i=0;i<nums.size();i++){
             sum+=nums[i];
-            if(sum>max){
+            if(max < sum){
                 max = sum;
-                if(sum<0){sum=0;}
+                if(sum<0){
+                    sum=0;
+                }
             }
-            if(sum<0){sum=0;}
+            if(sum<0){
+                sum=0;
+            }
         }
         return max;
     }
 };
-// this above algorithm is kadanes algorithm
