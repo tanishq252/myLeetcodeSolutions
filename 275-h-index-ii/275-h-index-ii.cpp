b32f0f96f8dd6b812cc9aafe70262dd8dda49926
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-07-26 20:51:29 +0530

    Time: 24 ms (68.79%), Space: 18.7 MB (5.49%) - LeetHub

--- 275-h-index-ii/275-h-index-ii.cpp
+++ 275-h-index-ii/275-h-index-ii.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-                int n = citations.size();
+        int n = citations.size();
+        //priority queue to store max H-index
         priority_queue<int> pq;
         sort(citations.begin(), citations.end());
-        int ind = lower_bound(citations.begin(), citations.end(), 2)-citations.begin();
         for(int i=0;i<=citations[n-1];i++){
+            //lower bound of current value 
             int lb = lower_bound(citations.begin(), citations.end(), i)-citations.begin();
-            if(i == n-lb or i==n-lb-1){
-                pq.push(i);         
-                // break;
-                }
+            //considering either the current value or the papers after current value
+            // if(i == n-lb or i==n-lb-1){
+            //     pq.push(i);         
+            // }
+            //upper bound of current value 
             int ub = upper_bound(citations.begin(), citations.end(), i)-citations.begin();
-            cout<<n-lb<<" "<<i<<"\n";
-            if(i == n-ub){
-                pq.push(i);         
-                // break;
-            }
+            // if(i == n-ub){
+            //     pq.push(i);         
+            // }
+            //considering the values between lower bound and upper found
             for(int j=n-ub;j<=n-lb;j++){
                 if(i == j){
                 pq.push(i);         
-                // break;
             }
             }
-            // cout<<i<<" "<<lb<<"\n";
         }
-        // cout<<pq.top();
         return (!pq.empty()) ? pq.top() : (citations[0] ==0) ? citations[n-1] : citations[0];
     }
 };
