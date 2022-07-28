12f587db64ef6d2aae8297e9540852daf969dc5a
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-07-28 07:59:33 +0530

    Time: 13 ms (59.68%), Space: 7.3 MB (47.02%) - LeetHub

---
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int a = s1.length();        
+        int b = s2.length();
+        if(a>b){
+            return false;
+        }
+        vector<int> window1(26, 0);        
+        vector<int> window2(26, 0);
+        for(int i=0;i<a;i++){
+            window1[s1[i]-'a']++;            
+            window2[s2[i]-'a']++;
+        }
+        
+        if (window1 == window2) return true;
+        
+        for(int i=a;i<b;i++){
+            window1[s2[i-a]-'a']++;            
+            window2[s2[i]-'a']++;
+            if (window1 == window2) return true;
+        }
+
+        return false;
+
+    }
+};
