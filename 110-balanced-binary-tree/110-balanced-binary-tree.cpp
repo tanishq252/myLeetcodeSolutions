bcba7e9a0fe0b2bc9ef713cc75ac6ec5d5c74fc0
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-07-29 15:30:47 +0530

    Time: 21 ms (50.26%), Space: 21.8 MB (8.60%) - LeetHub

---
+++ 110-balanced-binary-tree/110-balanced-binary-tree.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool ans = true;
+    
+    int findHeight(TreeNode *root){
+        if(root==NULL) return 0;
+        if(!ans) return 0;
+        if(root->left == NULL and root->right == NULL ) return 1;
+        int l=0, r=0;
+        if(root->left!=NULL) l = findHeight(root->left);        
+        if(root->right!=NULL) r = findHeight(root->right);
+        if(abs(l-r) > 1) ans = false;
+        return max(l, r)+1;
+    }
+    
+    void levelOrder(vector<vector<int>> &v, TreeNode *root, int depth){
+        if(root==NULL) return;
+        if(v.size() == depth) v.push_back({});
+        v[depth].push_back(root->val);
+        levelOrder(v, root->left, depth+1);        
+        levelOrder(v, root->right, depth+1);
+
+    }
+    
+    bool isBalanced(TreeNode* root) {
+        if(root==NULL) return true;
+        
+        int l = findHeight(root->left);        
+        int r = findHeight(root->right);
+        
+        if(abs(l-r)>1) ans = false;
+        
+        return ans;
+
+    }
+};
