43ad124059f7b942c4c26c6ede35436a06e339ee
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-07-31 19:25:31 +0530

    Time: 27 ms (35.53%), Space: 13.4 MB (18.42%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -22,16 +22,31 @@ public:
             for(int j=0;j<cols.size();j++){
                 if(matrix[i][j] == 0){
                     visited[i][j] = true;
+                    rows[i] = true; cols[j] = true;
                 }
             }
         }
         for(int i=0;i<rows.size();i++){
-            for(int j=0;j<cols.size();j++){
-                if(matrix[i][j] == 0 and visited[i][j]){
-                    setRowsCols(matrix, rows, cols, i, j);
+            if(rows[i]){
+                for(int x=0;x<cols.size();x++){
+                    matrix[i][x] = 0;
                 }
             }
         }
+        for(int i=0;i<cols.size();i++){
+            if(cols[i]){
+                for(int x=0;x<rows.size();x++){
+                    matrix[x][i] = 0;
+                }
+            }
+        }
+        // for(int i=0;i<rows.size();i++){
+        //     for(int j=0;j<cols.size();j++){
+        //         if(matrix[i][j] == 0 and visited[i][j]){
+        //             setRowsCols(matrix, rows, cols, i, j);
+        //         }
+        //     }
+        // }
         
     }
 };
