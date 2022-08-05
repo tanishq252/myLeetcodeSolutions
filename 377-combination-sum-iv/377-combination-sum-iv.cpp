6f75f794ea468e2e0178129c31bdce307012169b
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-08-05 08:27:59 +0530

    Time: 3 ms (62.90%), Space: 6.6 MB (65.95%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,15 +1,48 @@
 class Solution {
 public:
+
     int combinationSum4(vector<int>& nums, int target) {
         vector<uint> dp(target+1, 0);
         sort(nums.begin(), nums.end());
-        dp[0] = 1;
-        for(int i=1;i<dp.size();i++){
-            for(int j=0;j<nums.size();j++)
+        dp[0]=1;
+        for(int i=1;i<=target;i++){
+            for(int j=0;j<nums.size();j++){
                 if(nums[j]<=i){
                     dp[i]+=dp[i-nums[j]];
                 }
+            }
         }
         return dp[target];
     }
+    
+    // TLE
+//     int ans=0;
+//     void comboHelper(vector<int> &combo, vector<int> &nums, int target, int ind){
+//         if(ind == nums.size()) return;
+//         if(accumulate(combo.begin(), combo.end(), 0) > target){
+//             return;
+//         }else if(accumulate(combo.begin(), combo.end(), 0) == target){
+//             sort(combo.begin(), combo.end());
+//             do{
+//                 cout<<"\n";
+//                 for(auto i:combo){
+//                     cout<<i<<" ";
+//                 }
+//                 ans++;
+//             }while(next_permutation(combo.begin(), combo.end()));cout<<"\n";
+//             return;
+//         }
+        
+//         combo.push_back(nums[ind]);
+//         comboHelper(combo, nums, target, ind);
+//         combo.pop_back();
+//         comboHelper(combo, nums, target, ind+1);
+        
+//     }
+//     int combinationSum4(vector<int>& nums, int target) {
+//         vector<int> combo;
+//         sort(nums.begin(), nums.end());
+//         comboHelper(combo, nums, target, 0);
+//         return ans;
+//     }
 };
