aa05d03764cc9bdfcaff6c7e82d0ef5773e6ecb6
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-08-06 09:14:14 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (99.64%) - LeetHub

--- 458-poor-pigs/458-poor-pigs.cpp
+++ 458-poor-pigs/458-poor-pigs.cpp
@@ -4,14 +4,12 @@ public:
         int c=0;
         int k = 1;
         while(target > k){
-            cout<<c<<" ";
             k*=tests;
             c++;
         }
         return c;
     }
     int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
-        
         return findMaxFactor((minutesToTest/minutesToDie)+1, buckets);
     }
 };
