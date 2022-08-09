90d43a162240ef0637a6f701a0ee82c3e8081b8f
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-08-09 12:20:16 +0530

    Time: 538 ms (36.86%), Space: 148.9 MB (27.98%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        
-    vector<int> result;
-    if (k == 0) return result;
-    priority_queue<pair<int, int>> w;
-    for (int i = 0, n = (int)nums.size(); i < n; i++) {
-        while (!w.empty() && w.top().second <= i-k)
-            w.pop();
-        w.push(make_pair(nums[i],i));
-        if (i >= k-1)
-            result.push_back(w.top().first);
+        vector<int> ans;
+        int n = nums.size();
+        if(n == k) return {*max_element(nums.begin(), nums.end())};
+        if(k == 1) return nums;
+
+    priority_queue<pair<int, int>> pq;
+    for (int i = 0;i < n; i++) {
+        while(!pq.empty() and pq.top().second <= i-k ) pq.pop();
+        pq.push({nums[i], i});
+        if(i>=k-1){
+            ans.push_back(pq.top().first);
+        }
     }
-    return result;
+    return ans;
     }
         
         // brute force
