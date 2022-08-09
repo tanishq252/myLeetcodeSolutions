9ca4de42c2853fab014decfa216c323d6bccb73e
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-08-09 07:26:03 +0530

    Time: 2565 ms (5.23%), Space: 9.3 MB (54.77%) - LeetHub

--- 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
@@ -1,8 +1,14 @@
 class Solution {
 public:
+    // Provided logic comprises of working with factors of given number ans storing them in a hashmap which helps to achieve dynamic programming 
+    // Initially we create a map and keep on updating frequency as 1 for all elements in the given array, indicating that every element forms an independent tree
+    // Further we traverse through the whole array to check the factors of the element
+    // If we find one factor we have to check whether there is another factor available in the given array, whose product with current factor is the current element
+    // If we find such a pair then we multiply number of trees of both the elements and finally multiply it with 2 becasue one node can have 2 possibilities of trees, and add it to the number of trees of current element
+    // Further we check whether the square root of element is there or not, if it exists then we multiply the square root with itself and add it to number of trees
+    // Finally we calculate total number of trees for all elements and the above dicussed logic satisfies all the test cases
     int mod = 1000000007;
     int numFactoredBinaryTrees(vector<int>& arr) {
-        vector<int> dp(arr.size()-1, 0);
         map<int, long long> mp;
         sort(arr.begin(), arr.end());
         for(auto i:arr){
@@ -22,12 +28,13 @@ public:
             
         }
         
-        // 
+        //here int gives wrong answer since the number of trees can exceed the integer 
         long s = 0;
         for(auto i:mp){
             s+= (i.second);
         }
         
+        //mod of the final answer else it does not return an integer
         return s%mod;
     }
 };
