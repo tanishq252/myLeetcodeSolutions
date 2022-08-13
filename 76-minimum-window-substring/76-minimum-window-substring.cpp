45522bc4d0225463d27ee8f3bee6f9c2ed0ec112
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-08-13 22:46:22 +0530

    Time: 13 ms (86.16%), Space: 7.7 MB (68.31%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.cpp
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -3,14 +3,25 @@ public:
     string minWindow(string s, string t) {
         vector<int> map(128,0);
         for(auto c: t) map[c]++;
-        int counter=t.size(), begin=0, end=0, d=INT_MAX, head=0;
-        while(end<s.size()){
-            if(map[s[end++]]-->0) counter--; //in t
-            while(counter==0){ //valid
-                if(end-begin<d)  d=end-(head=begin);
-                if(map[s[begin++]]++==0) counter++;  //make it invalid
-            }  
+        int counter=t.size(), left=0, right=0, minLen=INT_MAX, minInd=0;
+        while(right<s.size()){
+            if(map[s[right]]>0){
+                counter--;
+            }
+            map[s[right]]--;
+            right++;
+            while(counter == 0){
+                if(right-left < minLen){
+                    minLen = right-left;
+                    minInd = left;
+                }
+                map[s[left]]++;
+                if(map[s[left]]>0){
+                    counter++;
+                }
+                left++;
+            }
         }
-        return d==INT_MAX? "":s.substr(head, d);
+        return minLen==INT_MAX? "":s.substr(minInd, minLen);
     }
 };
