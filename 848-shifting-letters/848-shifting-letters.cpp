bb02c549d04f96933426d09a86fc5091c2aadb3a
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-08-21 00:32:51 +0530

    Time: 167 ms (77.30%), Space: 73.3 MB (11.21%) - LeetHub

---
+++ 848-shifting-letters/848-shifting-letters.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    
+    string shiftingLetters(string s, vector<int>& shifts) {
+        int n = shifts.size();
+        string alp = "abcdefghijklmnopqrstuvwxyz";
+        vector<long long int> shift(n, 0);
+        int mod = 1000000009;
+        for(int i=0;i<n;i++){
+            shift[i] = shifts[i];
+        }
+        for(int i=n-2;i>=0;i--){
+            shift[i]+=(shift[i+1]);
+            // shift[i]%=mod;
+        }
+        for(int i=0;i<n;i++){
+            long long int j = s[i]-'a';
+            j+= shift[i];
+            // j%=mod;
+            j%=26;
+            // cout<<j<<" ";
+            s[i] = alp[j];
+        }
+        return s;
+    }
+};
