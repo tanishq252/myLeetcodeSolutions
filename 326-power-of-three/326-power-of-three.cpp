3e32b6c1fe9401581a19c9df7e5ed99299d6ed07
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-08-22 08:44:52 +0530

    Time: 24 ms (79.84%), Space: 6 MB (29.00%) - LeetHub

---
+++ 326-power-of-three/326-power-of-three.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n<=0) return false;
+        while(n>0){
+            if(n==1){
+                return true;
+            }
+            if(n%3!=0) return false;
+            n/=3;
+            
+        }
+        return true;
+    }
+};
