82f83511b1f7964d3c89760794ea99210a1f7dfa
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-08-22 08:43:53 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (24.97%) - LeetHub

---
+++ 342-power-of-four/342-power-of-four.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n<=0) return false;
+        while(n>0){
+            if(n==1){
+                return true;
+            }
+            if(n%4!=0) return false;
+            n/=4;
+            
+        }
+        return true;
+    }
+};
