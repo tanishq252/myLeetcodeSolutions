70b64d42523979300773471602e0e7c49a423cf7
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-08-26 13:57:22 +0530

    Time: 3 ms (82.92%), Space: 7.1 MB (54.16%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int myAtoi(string str) {
+            int myAtoi(string str) {
     int sign = 1, base = 0, i = 0;
     while (str[i] == ' ') { i++; }
     if (str[i] == '-' || str[i] == '+') {
@@ -14,48 +14,5 @@ public:
         base  = 10 * base + (str[i++] - '0');
     }
     return base * sign;
-        
-//         int sign = 0;bool neg = false;
-//         string ans = "";int i=0;
-//         while(i<s.length()){
-//             if(ans==""){
-//                 if(s[i]=='+' or s[i]=='-'){
-//                     if(s[i]=='-') {
-//                         neg = true;
-//                     }
-//                     sign++;
-//                     if(sign >= 2) return 0;
-//                 }
-//                 else if(s[i]-'0' >= 10 or s[i]-'0' < 0){
-//                     // if(ans==""){
-//                         break;
-//                     // }
-//                 }
-//             }
-//             else if(s[i]-'0' < 10 or s[i]-'0' >= 0){
-//                 ans.push_back(s[i]);
-//             }
-//             i++;
-//         }
-//         int mx = INT_MAX;
-//         i=0;long long int base = 0;
-//         while(i<ans.length()){
-//             int j = ans[i]-'0';
-//             base = base*10 + j;
-
-//             if(base >= mx){
-//                 if(neg){
-//                     return INT_MIN;
-//                 }else{
-//                     return INT_MAX;
-//                 }
-//             }
-//             i++;
-//         }
-//         if(neg){
-//             base*= -1;
-//         }
-//         cout<<ans;
-//         return base;
     }
 };
