9776df65f03f829c962660b0ea0b8d289dae0480
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2022-10-07 20:06:18 +0530

    Time: 794 ms (5.01%), Space: 43.6 MB (7.73%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.cpp
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -1,49 +1,23 @@
 class MyCalendar {
 public:
-    vector<vector<int>> bookings;
-    
+    map<long long int, int> bookings;    
     MyCalendar() {
         
     }
     
     bool book(int start, int end) {
-        for(auto i:bookings){
-            if(i[0]<end and start<i[1]) return false;
+        int c=0, mx=0;
+        bookings[start]++; 
+        bookings[end]--;
+        for(auto &i:bookings){
+            c+=i.second;
+            if(c==2) {
+                bookings[start]--; 
+                bookings[end]++;
+                return false;
+            }
         }
-        bookings.push_back({start, end});
         return true;
-        // 93/107 test cases passed
-        // if(bookings.size()==0) {bookings.push_back({start, end});return true;}
-        // else if(bookings.size()==1){
-        //     if(bookings.back()[1] <= start){
-        //         bookings.push_back({start, end});
-        //         sort(bookings.begin(), bookings.end());
-        //         return true;
-        //     }else if(bookings.front()[0] >= end){
-        //         bookings.push_back({start, end});
-        //         sort(bookings.begin(), bookings.end());
-        //         return true;
-        //     }
-        // }
-        // else if(bookings.size() >= 2){
-        //     for(int i=0;i<bookings.size()-1;i++){
-        //         if(bookings[i][1]<=start and bookings[i+1][0]>=end){
-        //             bookings.push_back({start, end});
-        //             sort(bookings.begin(), bookings.end());
-        //             return true;
-        //         }
-        //     }
-        //     if(bookings.back()[1] <= start){
-        //         bookings.push_back({start, end});
-        //         sort(bookings.begin(), bookings.end());
-        //         return true;
-        //     }else if(bookings.front()[0] >= end){
-        //         bookings.push_back({start, end});
-        //         sort(bookings.begin(), bookings.end());
-        //         return true;
-        //     }
-        // }
-        // return false;
     }
 };
 
