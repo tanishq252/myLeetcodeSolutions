f93487af9d8e315238f3b47cadb559373aa70ac2
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2023-01-28 10:52:28 +0530

    Time: 129 ms (25.88%), Space: 50.2 MB (5.10%) - LeetHub

--- 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.cpp
+++ 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.cpp
@@ -1,28 +1,31 @@
 class SummaryRanges {
 public:
     vector<vector<int>> ans;
-    set<int> vec;
+    set<int> v;
     
     SummaryRanges() {
         
     }
     
     void addNum(int value) {
-        vec.insert(value);
+        v.insert(value);
     }
     
     void mergeIntervals(){
-        ans = {};int i=-1,j=-1;
-        for(auto &v:vec){
-            if(i<0){
-                i=j=v;
-            }else if(v == j+1){
-                j=v;
-            }else{
-                ans.push_back({i,j});i=j=v;
+        ans = {};int i=0,j=0,fi=0;
+        vector<int> vec(v.begin(), v.end());
+        // if(vec.size())
+        while(i<vec.size()){
+            j=i+1;
+            if(j==vec.size()) break;
+            while(vec[j]==vec[j-1]+1){
+                j++;
+                if(j==vec.size()) break;
             }
+            ans.push_back({vec[i], vec[j-1]});
+            i=j;
         }
-        ans.push_back({i,j});
+        if(j!=i) ans.push_back({vec[i], vec[i]});
     }
     
     vector<vector<int>> getIntervals() {
