13f41f730989cee3b9629606dce0d9491248669d
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2023-01-30 07:42:26 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (6.99%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     int tribonacci(int n) {
-        vector<int> v = {0,1,1};
+        if(n==0) return 0;
+        if(n<=2) return 1;
+        vector<int> dp(n+1);
+        dp[0]=0, dp[1]=dp[2]=1;
         for(int i=3;i<=n;i++){
-            v.push_back(v[i-2]+v[i-1]+v[i-3]);
+            dp[i] = dp[i-1]+dp[i-2]+dp[i-3];
         }
-        return v[n];
+        return dp[n];
     }
 };
