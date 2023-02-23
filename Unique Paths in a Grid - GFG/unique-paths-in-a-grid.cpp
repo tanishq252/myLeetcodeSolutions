e066f296e2327526cc3d5ab3b7b80083e959ed16
Tanishq Deshpande <78098329+tanishq252@users.noreply.github.com>
2023-02-23 10:17:36 +0530

    Added solution - LeetHub

--- Unique Paths in a Grid - GFG/unique-paths-in-a-grid.cpp
+++ Unique Paths in a Grid - GFG/unique-paths-in-a-grid.cpp
@@ -10,7 +10,7 @@ class Solution {
   public:
     int uniquePaths(int n, int m, vector<vector<int>> &grid) {
         // code here
-        vector<vector<int>> dp(n, vector<int> (m,1)), ddp(n, vector<int> (m,1));
+        vector<vector<int>> dp;
         n-=1, m-=1;
         
         dp=grid;
@@ -18,31 +18,14 @@ class Solution {
         int mod = 1000000007;
         
         if(grid[n][m]==0) return 0;
-        // if(n==0){
-            for(int i=1;i<=m;i++){
-                if(grid[0][i]==0) dp[0][i]=0;
-                else dp[0][i]=dp[0][i-1];
-            }
-            // return 1;
-        // }
-        // if(m==0){
-            for(int i=1;i<=n;i++){
-                if(grid[i][0]==0) dp[i][0]=0;
-                else dp[i][0]=dp[i-1][0];
-            }
-        //     return 1;
-        // }
-         
-        // for(int i=0;i<=n;i++){
-        //     for(int j=0;j<=m;j++){
-        //         if(grid[i][j]==0){
-        //             dp[i][j]=0;
-        //         }
-        //     }
-        // }
-        
-        // dp = grid;
-
+        for(int i=1;i<=m;i++){
+            if(grid[0][i]==0) dp[0][i]=0;
+            else dp[0][i]=dp[0][i-1];
+        }
+        for(int i=1;i<=n;i++){
+            if(grid[i][0]==0) dp[i][0]=0;
+            else dp[i][0]=dp[i-1][0];
+        }
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
                 if(dp[i][j]!=0){
